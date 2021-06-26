4e887e057fb04f69d165837f20478eafe85ac662
walpizar <walpizar@hotmail.com>
2021-06-25 20:52:21 -0600

    clase 7

--- SEMANA2PRYECTO/proyectoApi/src/routes/usuarios.ts
+++ SEMANA2PRYECTO/proyectoApi/src/routes/usuarios.ts
@@ -8,10 +8,10 @@ const routes= Router();
 //apuntar a la accion del controlador
 
 routes.get('/',[checkJwt, checkRole(['admin','user'])], UsuariosController.obtenerTodos);
-routes.get('/:id', UsuariosController.obtenerPorId);
-routes.post('/', UsuariosController.guardar);
-routes.patch('/:id', UsuariosController.modificar);
-routes.delete('/:id',UsuariosController.eliminar);
+routes.get('/:id',[checkJwt, checkRole(['admin','user'])], UsuariosController.obtenerPorId);
+routes.post('/',[checkJwt, checkRole(['admin','user'])], UsuariosController.guardar);
+routes.patch('/:id',[checkJwt, checkRole(['admin','user'])], UsuariosController.modificar);
+routes.delete('/:id', UsuariosController.eliminar);
 
 export default routes;
 
